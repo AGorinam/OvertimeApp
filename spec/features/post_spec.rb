fd8a2171e22603ab917518ce98ac2601bf0825c3
Alejandro Gorina <alejandrogorinam@gmail.com>
2022-02-01 13:45:51 +0100

    FIX: add not saved test

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -7,7 +7,7 @@ describe 'navigate' do
       expect(page.status_code).to eq(200)
     end
 
-    it 'has a titile of Posts' do
+    it 'has a title of Posts' do
       visit posts_path
       expect(page).to have_content(/Posts/)
     end
