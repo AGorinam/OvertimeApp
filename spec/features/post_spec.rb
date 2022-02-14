573755d719ff132b405ec47283a8469e10bbe1f8
Alejandro Gorina <alejandrogorinam@gmail.com>
2022-02-14 15:39:09 +0100

    FIX: Took null false off from posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 describe 'navigate' do
   # Before doing all the test I am using Warden to make the app log in
   before do
-      user = User.create(
-        email: 'test@test.com',
-        password: '123123',
-        password_confirmation: '123123',
-        first_name: 'Jon',
-        last_name: 'Snow'
-      )
-      login_as(user)
-      visit new_post_path
+    user = User.create(
+      email: 'test@test.com',
+      password: '123123',
+      password_confirmation: '123123',
+      first_name: 'Jon',
+      last_name: 'Snow'
+    )
+    login_as(user, :scope => :user)
+    visit new_post_path
   end
   describe 'index' do
     it 'can be reached successfully' do
@@ -28,7 +28,7 @@ describe 'navigate' do
       post1 = Post.create(date: Date.today, rationale: 'Post 1')
       post2 = Post.create(date: Date.today, rationale: 'Post 2')
       visit posts_path
-      expect(page).to have_content(/Post1|Post2/)
+      expect(page).to have_content(/Post 1|Post 2/)
     end
   end
 
