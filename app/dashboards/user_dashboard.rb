8b16c41c3365a11495929be5745d83c75c8d49c4
Alejandro Gorina <alejandrogorinam@gmail.com>
2022-03-28 16:34:21 +0200

    FEAT: Updated forms in admin dashboard

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -11,6 +11,7 @@ class UserDashboard < Administrate::BaseDashboard
     posts: Field::HasMany,
     id: Field::Number,
     email: Field::String,
+    password: Field::String,
     encrypted_password: Field::String,
     reset_password_token: Field::String,
     reset_password_sent_at: Field::DateTime,
@@ -28,10 +29,10 @@ class UserDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = %i[
-    posts
     id
+    type
     email
-    encrypted_password
+    posts
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
@@ -55,15 +56,10 @@ class UserDashboard < Administrate::BaseDashboard
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = %i[
-    posts
     email
-    encrypted_password
-    reset_password_token
-    reset_password_sent_at
-    remember_created_at
     first_name
     last_name
-    type
+    password
   ].freeze
 
   # COLLECTION_FILTERS
